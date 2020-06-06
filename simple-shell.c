87f95974e58d9b39b248ab4ce1c29e1cf794f9aa
Gia Nguyen <nhgia.apcs@gmail.com>
2020-06-07 01:04:05 +0700

    Add concurrence feature

--- simple-shell.c
+++ simple-shell.c
@@ -4,14 +4,12 @@
  * 
  **/
 
-
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h> //calloc
 #include <sys/types.h>
 #include <sys/wait.h>
-
 
 // For clear screen
 #ifdef _WIN32
@@ -24,42 +22,42 @@
 #define MAX_LENGTH 80 // The maximum length of the commands
 
 void init_shell() //Open when launch shell
-{ 
-    clrscr(); 
-    printf("\n============================================"); 
-    printf("\n|              SIMPLE SHELL                |"); 
-    printf("\n____________________________________________"); 
-	printf("\n|  Project 2 - CS333 Spring 2020           |"); 
-	printf("\n|  Authors: Nguyen Hoang Gia (1751064)     |"); 
-	printf("\n|           Pham Bao Duy     (1751063)     |"); 
-    printf("\n============================================"); 
-	printf("\n"); 
-    char* username = getenv("USER"); 
-    printf("\nHello @%s, type help for menu.", username); 
-    printf("\n\n"); 
-} 
+{
+	clrscr();
+	printf("\n============================================");
+	printf("\n|              SIMPLE SHELL                |");
+	printf("\n____________________________________________");
+	printf("\n|  Project 2 - CS333 Spring 2020           |");
+	printf("\n|  Authors: Nguyen Hoang Gia (1751064)     |");
+	printf("\n|           Pham Bao Duy     (1751063)     |");
+	printf("\n============================================");
+	printf("\n");
+	char *username = getenv("USER");
+	printf("\nHello @%s, type help for menu.", username);
+	printf("\n\n");
+}
 
-void help_panel() 
-{ 
-    puts("\n____________________________________________"
+void help_panel()
+{
+	puts("\n____________________________________________"
 		 "\n|________________HELP PANEL________________|"
-         "\n|__________________________________________|"
-         "\n| Some commands supported:                 |"
-         "\n|    >!!                                   |"
-         "\n|    >ls                                   |"
+		 "\n|__________________________________________|"
+		 "\n| Some commands supported:                 |"
+		 "\n|    >!!                                   |"
+		 "\n|    >ls                                   |"
 		 "\n|    >pwd                                  |"
 		 "\n|    >ping                                 |"
-         "\n|    >exit                                 |"
-         "\n|__________________________________________|\n"); 
-  
-    return; 
-} 
+		 "\n|    >exit                                 |"
+		 "\n|__________________________________________|\n");
+
+	return;
+}
 
 void parse(char *command, char **args)
 {
-	while (*command !='\0') //if not the end of line, continue
+	while (*command != '\0') //if not the end of line, continue
 	{
-		while(*command == ' ' || *command == '\t' || *command == '\n')
+		while (*command == ' ' || *command == '\t' || *command == '\n')
 		{
 			*command++ = '\0'; //replace white spaces with 0
 		}
@@ -74,109 +72,128 @@ void parse(char *command, char **args)
 
 int findAmpersand(char **args)
 {
-    //Find "&"
-    int count = 0;
-    while (args[count] != NULL && strcmp(args[count], "&") != 0)
-        count++;
-    
-    if (args[count] != NULL)
+	//Find "&"
+	int count = 0;
+	while (args[count] != NULL && strcmp(args[count], "&") != 0)
+		count++;
+
+	if (args[count] != NULL)
+	{
 		//Found
-        return 1;
-    return 0;
+		args[count] = NULL; // Remove "&" to ensure that fork() will run properly.
+		return 1;
+	};
+	return 0;
 }
 
 int findPipe(char **args)
 {
-    //Find "|""
-    int count = 0;
-    while (args[count] != NULL && strcmp(args[count], "|") != 0)
-        count++;
-    
-    if (args[count] != NULL)
+	//Find "|""
+	int count = 0;
+	while (args[count] != NULL && strcmp(args[count], "|") != 0)
+		count++;
+
+	if (args[count] != NULL)
 		//Found
-        return 1;
-    return 0;
+		return 1;
+	return 0;
 }
 
-void exec_w_Pipe(char** args) 
-{ 
-	if (findPipe(args) != 1) {
-		printf("\n*** PIPE: error, doing nothing."); 
+void exec_w_Pipe(char **args)
+{
+	if (findPipe(args) != 1)
+	{
+		printf("\n*** PIPE: error, doing nothing.");
 		return;
 	}
 	//Pre-process: Take argument before "|" and after.
-	char* parsed[MAX_LENGTH]; 
-	char* parsedpipe[MAX_LENGTH]; 
+	char *parsed[MAX_LENGTH];
+	char *parsedpipe[MAX_LENGTH];
 
 	int countTemp = 0, foundPipe = 0, argLen = 0, pipeLen = 0;
-    while (args[countTemp] != NULL) {
+	while (args[countTemp] != NULL)
+	{
 		if (strcmp(args[countTemp], "|") == 0)
 			foundPipe = 1;
-		if (foundPipe == 0) {
+		if (foundPipe == 0)
+		{
 			parsed[argLen] = args[countTemp];
 			argLen++;
 		}
-		else if (strcmp(args[countTemp], "|") != 0) {
+		else if (strcmp(args[countTemp], "|") != 0)
+		{
 			parsedpipe[pipeLen] = args[countTemp];
 			pipeLen++;
 		};
 		countTemp++;
 	};
-    
+
+	// Mark the end of char array by "end" character
 	parsed[argLen] = '\0';
 	parsedpipe[pipeLen] = '\0';
 
-    // Define:
+	// Define:
 	// fd[0] is read end, fd[1] is write end
-    int fd[2];
-    pid_t pipe1, pipe2; 
-  
-    if (pipe(fd) < 0) { 
-        printf("\n*** PIPE: Init error."); 
-        return; 
-    } 
-    pipe1 = fork(); 
-    if (pipe1 < 0) { 
-        printf("\n*** PIPE: Fork error."); 
-        return; 
-    } 
-  
-    if (pipe1 == 0) { 
-        // pipe1
-        // write at fd[1] 
-        close(fd[0]); 
-        dup2(fd[1], STDOUT_FILENO); //duplicate
-        close(fd[1]); 
-  
-        if (execvp(parsed[0], parsed) < 0) { 
-            printf("\n*** PIPE: Error when executing command befor pipe."); 
-            exit(0); 
-        } 
-    } else { 
-        // Parent execute 
-        pipe2 = fork(); 
-  
-        if (pipe2 < 0) { 
-            printf("\n*** PIPE: Fork parent error"); 
-            return; 
-        } 
-  
-        // Child 2 execute.
-        // read at fd[0] 
-        if (pipe2 == 0) { 
-            close(fd[1]); 
-            dup2(fd[0], STDIN_FILENO); //duplicate
-            close(fd[0]); 
-            if (execvp(parsedpipe[0], parsedpipe) < 0) { 
-                printf("\n*** PIPE: Error when executing command after pipe."); 
-                exit(0); 
-            } 
-        } else { 
-            // parent executing, waiting for two children 
-            wait(NULL); 
-        } 
-    } 
-} 
+	int fd[2];
+	pid_t pipe1, pipe2;
+
+	if (pipe(fd) < 0)
+	{
+		printf("\n*** PIPE: Init error.");
+		return;
+	}
+	pipe1 = fork();
+	if (pipe1 < 0)
+	{
+		printf("\n*** PIPE: Fork error.");
+		return;
+	}
+
+	if (pipe1 == 0)
+	{
+		// pipe1
+		// write at fd[1]
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO); //duplicate
+		close(fd[1]);
+
+		if (execvp(parsed[0], parsed) < 0)
+		{
+			printf("\n*** PIPE: Error when executing command befor pipe.");
+			exit(0);
+		}
+	}
+	else
+	{
+		// Parent execute
+		pipe2 = fork();
+
+		if (pipe2 < 0)
+		{
+			printf("\n*** PIPE: Fork parent error");
+			return;
+		}
+
+		// Child 2 execute.
+		// read at fd[0]
+		if (pipe2 == 0)
+		{
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO); //duplicate
+			close(fd[0]);
+			if (execvp(parsedpipe[0], parsedpipe) < 0)
+			{
+				printf("\n*** PIPE: Error when executing command after pipe.");
+				exit(0);
+			}
+		}
+		else
+		{
+			// parent executing, waiting for two children
+			wait(NULL);
+		}
+	}
+}
 
 void execute(char **args)
 {
@@ -186,17 +203,15 @@ void execute(char **args)
 	int isPipe = findPipe(args);
 	int isConcurrence = findAmpersand(args);
 
-	if (isPipe == 1) {
+	if (isPipe == 1)
+	{
 		//Execute if find pipe
 		exec_w_Pipe(args);
-	}
-	else if (isConcurrence == 1) {
-		//Execute concurrence
 	}
 	else
 	{
 		//Single command
-		if ((pid = fork()) < 0)
+		if ((pid = fork()) < 0) // fork child process
 		{
 			printf("*** ERROR: forking child process failed\n");
 			exit(1);
@@ -211,25 +226,38 @@ void execute(char **args)
 		}
 		else
 		{
-			while (wait(&status) != pid)
+			// Execute at parent process
+			if (isConcurrence == 1)
+			{
+				// Found "&"
+				//Execute concurrence, immediately go back to parent
+				return;
+			};
+
+			// Not found "&", waiting for child
+			while (pid != wait(&status))
+			{
 				printf("...\n");
+			};
 		}
 	}
 }
 
-int main(void) {
+int main(void)
+{
 
 	char command[MAX_LENGTH];
 	char prevCommand[MAX_LENGTH];
-	char *args[MAX_LENGTH/2 + 1]; // MAximum 40 argments
+	char *args[MAX_LENGTH / 2 + 1]; // MAximum 40 argments
 	prevCommand[0] = '\0';
 	int should_run = 1;
 	init_shell();
-	while (should_run) {
+	while (should_run)
+	{
 		printf("ssh>> ");
 		fflush(stdout);
 		fgets(command, MAX_LENGTH, stdin);
-		command[strlen(command)-1] = '\0';
+		command[strlen(command) - 1] = '\0';
 		if (strcmp(command, "") == 0)
 			continue;
 		else if (strcmp(command, "!!") == 0)
@@ -247,11 +275,13 @@ int main(void) {
 			exit(0);
 		else if (strcmp(args[0], "help") == 0)
 			help_panel();
-		else {
+		else if (strcmp(args[0], "clear") == 0)
+			clrscr();
+		else
+		{
 			strncpy(prevCommand, command, strlen(command) + 1);
 			execute(args);
 		}
 	}
 	return 0;
 }
-
